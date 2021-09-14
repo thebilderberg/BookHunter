04806616665e43235e1ff956bb3429bbbe052e35
thebilderberg <mygooglegirls@yandex.ru>
2021-09-14 13:52:26 +0300

    vol.3

--- bookHunter.js
+++ bookHunter.js
@@ -2,15 +2,25 @@
 
 
 
-function enterUserText(buttonEnter, window, buttonKey) {
+function enterUserText(buttonEnter, window, buttonKeyOne, buttonKeyTwo, buttonKeyThree) {
     let clickButtonFoot = document.querySelector(buttonEnter);
     clickButtonFoot.addEventListener('click', () => {
         let userText = document.querySelector(window);
         if (userText.value === 'Достоевский') {
             alert('Отлично, вы получаете ключ!');
-            let buttonOneKey = document.querySelector(buttonKey);
+            let buttonOneKey = document.querySelector(buttonKeyOne);
             buttonOneKey.addEventListener('click', () => {
                 alert('Промокод на маленький хуй');
+            });
+
+            let buttonTwoKey = document.querySelector(buttonKeyTwo);
+            buttonTwoKey.addEventListener('click', () => {
+                alert('Промокод на огурец');
+            });
+
+            let buttonThreeKey = document.querySelector(buttonKeyThree);
+            buttonThreeKey.addEventListener('click', () => {
+                alert('Промокод на пельмень');
             });
         } else if (userText.value === ''){
             alert('Нужно что-то ввести, чел!');
@@ -21,4 +31,4 @@ function enterUserText(buttonEnter, window, buttonKey) {
     });
 }
 
-enterUserText('.buttonFoot', '.inputWindow', '.buttonHeaderOne');
+enterUserText('.buttonFoot', '.inputWindow', '.buttonHeaderOne', '.buttonHeaderTwo', '.buttonHeaderThree');
